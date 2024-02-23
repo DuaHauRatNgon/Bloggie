10c78e8d229f23032fba1154a0b257d894f0c6a3
Dua Hau Rat Ngon <nmhuongbn@gmail.com>
2024-02-23 20:49:33 +0700

    manage, display, create, delete user

---
+++ Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Bloggie.Web.Models.ViewModels {
+    public class UserViewModel {
+        //prop de hien thi danh sach user 
+        public List<User> Users { get; set; }
+
+        //prop de bind vao form tao moi user
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool AdminRoleCheckbox { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -40,6 +40,7 @@ namespace Bloggie.Web {
             builder.Services.AddScoped<IBlogPostLikeRepository, BlogPostLikeRepository>();
             builder.Services.AddScoped<IBlogPostLikeRepository, BlogPostLikeRepository>();
             builder.Services.AddScoped<IBlogPostCommentRepository, BlogPostCommentRepository>();
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
 
             var app = builder.Build();
 
