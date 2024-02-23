dfebd91a903e8ec97c3fcbb447fad18d5797f89d
Dua Hau Rat Ngon <nmhuongbn@gmail.com>
2024-02-23 17:54:36 +0700

    about, contact

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -37,6 +37,13 @@ namespace Bloggie.Web.Controllers {
         public IActionResult Privacy() {
             return View();
         }
+        public IActionResult About() {
+            return View();
+        }
+
+        public IActionResult Contact() {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
